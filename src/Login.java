1dd9634690714129c926ccb6bb8b14b4b1534e22
Ashikhashree <231501021@rajalakshmi.edu.in>
2024-11-19 19:31:04 +0530

    Add files via upload

---
+++ src/Login.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package login;
+
+
+//.abstract ..class .abstract .abstract ..class ..class .abstract ..class ..class ..class ..class .abstract .abstract ..class ...abstract .abstract .
+public class Login {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        log obj1=new log();
+        obj1.setVisible(true);
+        obj1.pack();
+        obj1.setLocationRelativeTo(null);
+        
+    
+        
+    }
+}
